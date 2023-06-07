cd56da3dae992b2759cecc40c302524986140cd2
Peregrinus <zakharov.mark22@gmail.com>
2023-06-07 23:33:36 +0300

    naming refactor

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const { login, register, current } = require('../controllers/users');
 router.post('/login', login);
 
