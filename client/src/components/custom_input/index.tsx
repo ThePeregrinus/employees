3aa5afeac1a93769c1a56d992b22d501df76a937
Peregrinus <zakharov.mark22@gmail.com>
2023-09-30 00:20:01 +0300

    add CustomInput

---
+++ client/src/components/custom_input/index.tsx
@@ -0,0 +1,19 @@
+import { Input, Form } from 'antd';
+
+type Props = {
+  name?: string;
+  placeholder?: string;
+  type?: string;
+};
+
+export const CustomInput = ({ name, placeholder, type = 'text' }: Props) => {
+  return (
+    <Form.Item
+      name={name}
+      rules={[{ required: true, message: 'Обязательное поле' }]}
+      shouldUpdate={true}
+    >
+      <Input placeholder={placeholder} type={type} size="large" />
+    </Form.Item>
+  );
+};
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Card, Form, Row } from 'antd';
+
 import { Layout } from '../../components/layout';
 
 export const Login = () => {
   return (
     <Layout>
-      <div>Login</div>
+      <Row align="top" justify="center">
+        <Card title="Войдите" style={{ width: '30rem' }}>
+          <Form onFinish={() => null}></Form>
+        </Card>
+      </Row>
     </Layout>
   );
 };
